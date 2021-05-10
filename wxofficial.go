d81cf5673f937cc087c1c1923b4f835780f7efe3
root <root@ideal-gig-1.localdomain>
2021-05-10 15:15:59 +0800

    go mod init

---
+++ wxofficial.go
@@ -0,0 +1,13 @@
+package wxofficial
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	
+)
+
+func do() {
+	gin.Default()
+	fmt.Println("as")
+}
+
---
+++ wxofficial_test.go
@@ -0,0 +1,10 @@
+package wxofficial
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	do()
+	t.Errorf("AAA")
+}
